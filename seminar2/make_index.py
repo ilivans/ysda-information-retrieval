ad54fab7b26ee9828597188edf8b1f5661ed2f4c
Ilya Ivanov <ilya.ivanov.se@gmail.com>
2017-02-23 03:36:12 +0300

    dataframe value of inverted index

--- seminar2/make_index.py
+++ seminar2/make_index.py
@@ -2,11 +2,16 @@
 import os
 import cPickle
 import argparse
-from collections import defaultdict
+from collections import defaultdict, Counter
 
-from nltk.tokenize import RegexpTokenizer
+import pandas as pd
+import numpy as np
+from numpy import log2
+from scipy.special import expit
 
-tokenize = RegexpTokenizer("\w+").tokenize
+from utils import get_terms
+
+EPSILON = 1e-3
 
 
 def build_index(dir_path):
@@ -16,19 +21,40 @@ def build_index(dir_path):
     for document_name in os.listdir(dir_path):
         if document_name.endswith(".txt"):
             document_path = os.path.join(dir_path, document_name)
-            id_to_path[document_id] = document_path
             with open(document_path) as f:
                 index[document_id] = f.read()
+            id_to_path[document_id] = document_path
             document_id += 1
     return index, id_to_path
 
 
 def build_inverted_index(index):
-    inverted_index = defaultdict(lambda: set())
-    for document_id, content in index.iteritems():
-        for word in tokenize(content):
-            inverted_index[word.lower()].add(document_id)
-    return dict(inverted_index)
+    inverted_index = defaultdict(lambda: list())
+    for document_id, document in index.iteritems():
+        tokens = get_terms(document)
+        terms_frequencies = Counter(tokens)
+        for term, frequency in terms_frequencies.iteritems():
+            tf = float(frequency) / (len(tokens) + EPSILON)  # 0 <= tf < 1
+            inverted_index[term].append([document_id, tf])
+
+    # Inverted index with DataFrame as a value
+    inverted_index_df = dict()
+    for term, doc_ids_and_frequencies in inverted_index.iteritems():
+        inverted_index_df[term] = pd.DataFrame(doc_ids_and_frequencies, columns=["doc_id", term])
+
+    return inverted_index_df
+
+
+def get_documents_number(dir_path):
+    return len(filter(lambda name: name.endswith(".txt"), os.listdir(dir_path)))
+
+
+def get_inverse_document_frequencies(inverted_index, num_docs):
+    term_to_idf = dict()
+    for term, doc_ids_and_frequencies in inverted_index.iteritems():
+        num_docs_local = doc_ids_and_frequencies.shape[0]
+        idf = 2 * expit(log2(max(float(num_docs) / (num_docs_local + EPSILON), 1.))) - 1  # 0 <= idf < 1
+        term_to_idf[term] = idf
 
 
 def main():
@@ -39,7 +65,9 @@ def main():
 
     index, id_to_path = build_index(dir_path)
     inverted_index = build_inverted_index(index)
-    cPickle.dump((inverted_index, id_to_path), open("iindex.pkl", "wb"))
+    num_docs = get_documents_number(dir_path)
+    term_to_idf = get_inverse_document_frequencies(inverted_index, num_docs)
+    cPickle.dump((inverted_index, id_to_path, term_to_idf), open("iindex.pkl", "wb"))
 
 
 if __name__ == "__main__":
